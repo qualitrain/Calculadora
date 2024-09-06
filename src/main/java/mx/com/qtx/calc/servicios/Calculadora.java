72ca84bb729c8a2e962e1bf9ebf44d7b3e271808
acruzMaqRoja <qualitrain@gmail.com>
2024-09-06 17:39:50 -0600

    Calculadora funcionando con suma
    Soporta suma de enteros exclusivamente

---
+++ src/main/java/mx/com/qtx/calc/servicios/Calculadora.java
@@ -0,0 +1,9 @@
+package mx.com.qtx.calc.servicios;
+
+public class Calculadora {
+	public int sumar(int x, int y) {
+		int suma = x + y;
+		return suma;
+	}
+
+}
---
+++ src/main/java/mx/com/qtx/calc/test/CalculadoraTests.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.calc.test;
+
+import mx.com.qtx.calc.servicios.Calculadora;
+
+public class CalculadoraTests {
+
+	public static void main(String[] args) {
+		Calculadora miMaizoro = new Calculadora();
+		int resultado = miMaizoro.sumar(100, 150);
+		System.out.println("Resultado suma =" + resultado);
+	}
+
+}
