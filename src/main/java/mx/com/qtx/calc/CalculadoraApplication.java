b6fb5864b76041d07c92b7647b40a3933db8a4be
acruzMaqRoja <qualitrain@gmail.com>
2024-09-06 17:22:52 -0600

    Commit inicial

---
+++ src/main/java/mx/com/qtx/calc/CalculadoraApplication.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.calc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/qtx/calc/CalculadoraApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.qtx.calc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculadoraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
