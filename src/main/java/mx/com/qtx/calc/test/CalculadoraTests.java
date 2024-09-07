9946d1935ddc62b570a85aa71b076e5f5d610c72
acruzMaqRoja <qualitrain@gmail.com>
2024-09-06 19:59:21 -0600

    Test suma factorizado y funcionando

--- src/main/java/mx/com/qtx/calc/test/CalculadoraTests.java
+++ src/main/java/mx/com/qtx/calc/test/CalculadoraTests.java
@@ -5,6 +5,10 @@ import mx.com.qtx.calc.servicios.Calculadora;
 public class CalculadoraTests {
 
 	public static void main(String[] args) {
+		test_suma();
+	}
+
+	private static void test_suma() {
 		Calculadora miMaizoro = new Calculadora();
 		int resultado = (int) miMaizoro.sumar(100, 150);
 		System.out.println("Resultado suma =" + resultado);
